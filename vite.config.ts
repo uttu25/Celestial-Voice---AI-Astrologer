1aefed10e1499488d8fdbbd8d943a36690a4233c
Utkarsh Singh <86329478+uttu25@users.noreply.github.com>
2026-01-11 08:00:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // THIS LINE IS CRITICAL FOR ANDROID:
-  base: './', 
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
   },
-  server: {
-    port: 3000,
-  }
 });
